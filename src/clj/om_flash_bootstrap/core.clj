a7134f0281511fab12f0d18c9e22f657f8923900
Daniel Szmulewicz <daniel.szmulewicz@gmail.com>
2017-01-02 18:28:45 +0200

    Flash middleware
    - Also docstrings

--- src/clj/om_flash_bootstrap/core.clj
+++ src/clj/om_flash_bootstrap/core.clj
@@ -2,7 +2,10 @@
   (:require [ring.util.response :refer [response content-type]]
             [compojure.core :refer [routes GET]]))
 
-(defn flash-message [{session :session :as request}]
+
+(defn flash-message
+  "Flash handler"
+  [{session :session :as request}]
   (if-let [message (:message session)]
     (let [session (dissoc session :message)]
       (-> (pr-str message)
@@ -13,6 +16,24 @@
         response
         (content-type "application/edn"))))
 
-(defn flash [_]
+
+
+(defn flash
+  "Flash route"
+  [_]
   (routes
    (GET "/flash" [] flash-message)))
+
+(defn wrap-message
+  "Flash middleware"
+  [handler]
+  (fn [{flash :flash :as request}]
+    (if flash
+      (let [resp (handler request)
+            session (if (:session resp)
+                      (-> (:session resp)
+                          (assoc :message flash))
+                      (-> (:session request)
+                          (assoc :message flash)))]
+        (assoc resp :session session))
+      (handler request))))
